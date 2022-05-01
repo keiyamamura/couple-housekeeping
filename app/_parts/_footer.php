9284ef57c222adb14da15ce19c389e163c761a16
sanson0bass <maki4ma6_w_199san@yahoo.co.jp>
2022-05-01 22:57:48 +0900

    _header.php _footer.phpに記載した?<?php echo time(); ?>を削除

--- app/_parts/_footer.php
+++ app/_parts/_footer.php
@@ -10,7 +10,7 @@
 		</footer>
 		<?php if ($page_url !== $check_url['join']) : ?>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-			<script src="../public/js/main.js?<?php echo time(); ?>"></script>
+			<script src="../public/js/main.js"></script>
 		<?php endif; ?>
 		</body>
 
