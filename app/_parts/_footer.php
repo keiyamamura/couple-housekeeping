549426991827c456b5efe486e138780d4d91c95a
sanson0bass <maki4ma6_w_199san@yahoo.co.jp>
2022-05-09 23:01:15 +0900

    _footer.phpのscriptタグを変更

--- app/_parts/_footer.php
+++ app/_parts/_footer.php
@@ -8,8 +8,8 @@
 				</h1>
 			</div>
 		</footer>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 		<?php if ($page_url !== $check_url['join']) : ?>
-			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 			<script src="js/main.js"></script>
 		<?php endif; ?>
 		</body>
