88211ee85fdf25fbcfced74bf55b6a66416cafa9
sanson0bass <maki4ma6_w_199san@yahoo.co.jp>
2022-05-10 22:08:29 +0900

    _footer.phpのscriptタグのパス変更

--- app/_parts/_footer.php
+++ app/_parts/_footer.php
@@ -11,7 +11,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <?php if ($current_full_url !== $check_url['join']) : ?>
-	<script src="../public/js/main.js?<?php echo time(); ?>"></script>
+	<script src="js/main.js?<?php echo time(); ?>"></script>
 <?php endif; ?>
 </body>
 
