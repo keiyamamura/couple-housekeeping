2ac5d755488b5d18f8d9df7f6514d6e1131b584b
sanson0bass <maki4ma6_w_199san@yahoo.co.jp>
2022-05-11 21:38:45 +0900

    join/check.phpの登録するボタンをinputタグからbuttonタグへ変更

--- public/join/check.php
+++ public/join/check.php
@@ -71,7 +71,7 @@ require_once(__DIR__ . '/../../app/_parts/_header.php');
 		<div class="check-buttons">
 			<a href="index.php?action=rewrite" class="btn btn-outline-success">やり直す</a>
 			<span>|</span>
-			<input type="submit" class="btn btn-outline-success" value="登録する">
+			<button class="btn btn-outline-success">登録する</button>
 	</form>
 </section>
 
